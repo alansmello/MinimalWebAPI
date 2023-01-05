e35bb02d46002e781a17631848efe77e4b0f7f48
alansmello <alansmello@gmail.com>
2023-01-05 18:55:57 -0300

    Refactoring the constructor for more validations

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -9,14 +9,20 @@ namespace IWantApp.Domain.Products;
 public class Category : Entity
 {
    
-  public Category(string name)
+  public Category(string name, string createdBy, string editedBy)
     {
         var contract = new Contract<Category>()
-        .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório!");
+          .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório!")
+          .IsNotNullOrEmpty(createdBy, "CreatedBy")
+          .IsNotNullOrEmpty(editedBy, "EditedBy");
         AddNotifications(contract);
 
         Name = name;
         Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
     }
 
     public string Name { get; set; }
