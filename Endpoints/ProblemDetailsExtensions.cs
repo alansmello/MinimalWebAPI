ccc4bb5ace763c9973773e3a21d816021705a714
alansmello <alansmello@gmail.com>
2023-01-05 19:37:00 -0300

    created an extension method in the ProblemDetailsExtensions class

---
+++ Endpoints/ProblemDetailsExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flunt.Notifications;
+
+namespace IWantApp.Endpoints;
+
+public static class ProblemDetailsExtensions
+{
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notifications)
+    {
+        return notifications
+            .GroupBy(g => g.Key)
+            .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+    }    
+}
