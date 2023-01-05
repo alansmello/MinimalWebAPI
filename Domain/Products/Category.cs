24414d1775f333297584c04b576f3e331ea006c7
alansmello <alansmello@gmail.com>
2023-01-05 19:55:23 -0300

    validating  CategoryPut and refactoring Categories

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -8,26 +8,37 @@ namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-   
+   public string Name { get; private set; }
+  public bool Active { get; private set; }
+
   public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>()
-          .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório!")
-          .IsGreaterOrEqualsThan(name, 3, "Name", "Nome deve ter no mínimo 3 caracteres!")
-          .IsNotNullOrEmpty(createdBy, "CreatedBy")
-          .IsNotNullOrEmpty(editedBy, "EditedBy");
-        AddNotifications(contract);
-
         Name = name;
         Active = true;
         CreatedBy = createdBy;
         EditedBy = editedBy;
         CreatedOn = DateTime.Now;
         EditedOn = DateTime.Now;
+
+        Validate();             
     }
 
-    public string Name { get; set; }
-    public bool Active { get; set; }
+  public void Validate()
+  {
+     var contract = new Contract<Category>()
+          .IsNotNullOrEmpty(Name, "Name", "Nome é obrigatório!")
+          .IsGreaterOrEqualsThan(Name, 3, "Name", "Nome deve ter no mínimo 3 caracteres!")
+          .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
+          .IsNotNullOrEmpty(EditedBy, "EditedBy");
+        AddNotifications(contract);
+  }
+    public void EditInfo(string name, bool active)
+    {
+      Active = active;
+      Name = name;
+
+      Validate();
+    }
 
    
 }
