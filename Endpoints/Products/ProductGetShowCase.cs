cbd4ad9f5060d65e56292492fa47d00a82f55bc5
alansmello <alansmello@gmail.com>
2023-03-30 21:46:12 -0300

    finished to implement the endpoint ProductGetShowCase

--- Endpoints/Products/ProductGetShowCase.cs
+++ Endpoints/Products/ProductGetShowCase.cs
@@ -11,11 +11,28 @@ public class ProductGetShowCase
     public static string[] Methods => new string[] { HttpMethod.Get.ToString()};
     public static Delegate Handle => Action;
 
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action( ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
     {
-        var products = context.Products.Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name).ToList();
+       if (row > 10)
+        return Results.Problem(title:"Row with max 10", statusCode: 400);
+
+        var queryBase = context.Products.AsNoTracking().Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+        if(orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        if(orderBy == "price")
+            queryBase = queryBase.OrderBy(p => p.Price); 
+        else
+            return Results.Problem(title:"Order only by name or price", statusCode: 400);
+
+
+        var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
+
+        
+           
+
+        var products = queryFilter.ToList();
 
         var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
 
